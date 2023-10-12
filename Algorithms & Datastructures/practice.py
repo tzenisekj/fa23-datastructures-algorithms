4010a554c47d1ec6bba3170de3d8eff10cea7988
Tyler Zenisek <tzenisekj@gmail.com>
2023-10-12 14:07:04 -0500

    completed word assignment

--- Algorithms & Datastructures/practice.py
+++ Algorithms & Datastructures/practice.py
@@ -1,15 +1,8 @@
-import sys
 
-if __name__ == "__main__":
-    data = []
-    for i in range(20):
-        data.append(None)
+def second_to_last(self):
+    if self._size > 1:
+        node = self._head
+        while node.next.next != None:
+                node = node.next
 
-    past_size = sys.getsizeof(data)
-    while len(data) != 0:
-        a = len(data)
-        b = sys.getsizeof(data)
-        print("Length: {0:3d}: Size in bytes: {1:4d}".format(a, b))
-        data.pop()
-        if sys.getsizeof(data) < past_size:
-            print("Size Deducted")
+        return node
